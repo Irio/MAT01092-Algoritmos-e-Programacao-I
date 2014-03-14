04d70ae9c1ff3b99ec240a2da1f9b1a8adbebbd7
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-03-14 07:39:08 -0300

    Move each calculation to a function

--- pressao_atmosferica.f95
+++ pressao_atmosferica.f95
@@ -1,4 +1,4 @@
-program pressao_atmosferica
+program air_information
 implicit none
 
 real :: rho, T, p, g, h
@@ -8,22 +8,49 @@ real, parameter :: p0 = 101325   ! Sea level standard atmospheric pressure
 real, parameter :: T0 = 288.15   ! Sea level standard temperature
 real, parameter :: L  = .0065    ! Temperature lapse rate
 real, parameter :: RE = 6371     ! Earth's mean radius
+
+interface
+  real function t_in_celcius(t_in_kelvin) result (t)
+    real, intent(in) :: t_in_kelvin
+  end function t_in_celcius
+end interface
+
 	
   print *, '# To calculate density/temperature/pressure I need a height(m):'
   read  *, h
 
-  g   = 9.80665 * (RE / (RE + h)) ** 2                 ! Earth gravitational acceleration
-  p   = p0 * (1 - (L * h) / T0) ** ((g * M) / (R * L)) ! Pressure
-  T   = T0 - L * h                                     ! Absolute temperature
-  rho = (p * M) / (R * T)                              ! Density of gas
+  g   = earth_gravitational_acceleration(h)
+  p   = atmospheric_pressure(h, g)
+  T   = absolute_temperature(h)
+  rho = density_of_gas(p, T)
 
   print *, 'Density: ',     rho,             ' kg/m^3'
   print *, 'Temperature: ', t_in_celcius(t), ' ºC'
   print *, 'Pressure: ',    p,               ' Pa'
 
 contains
-  real function t_in_celcius(t_in_kelvin) result (t)
-  real, intent(in) :: t_in_kelvin
-    t = t_in_kelvin - 273.15
-  end function t_in_celcius
+  real function earth_gravitational_acceleration(h) result (g)
+  real, intent(in) :: h
+    g = 9.80665 * (RE / (RE + h)) ** 2
+  end function earth_gravitational_acceleration
+
+  real function atmospheric_pressure(h, g) result (p)
+  real, intent(in) :: g, h
+    p = p0 * (1 - (L * h) / T0) ** ((g * M) / (R * L))
+  end function atmospheric_pressure
+
+  real function absolute_temperature(h) result (T)
+  real, intent(in) :: h
+    T = T0 - L * h
+  end function absolute_temperature
+
+  real function density_of_gas(p, T) result (rho)
+  real, intent(in) :: p, T
+    rho = (p * M) / (R * T)
+  end function density_of_gas
 end program
+
+real function t_in_celcius(t_in_kelvin) result (t)
+real, intent(in) :: t_in_kelvin
+  t = t_in_kelvin - 273.15
+end function t_in_celcius
